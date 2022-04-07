30718e5139b4269adf59f23ddaf39a79a401b597
ehanschen <ehanschen@3pc.de>
2022-04-07 13:36:59 +0200

    template refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,35 @@
 <template>
   <div class="home">Helloooo</div>
-  <p>My name is {{ name }} and my age is {{ age }}</p>
+  <p ref="para">My name is {{ name }} and my age is {{ age }}</p>
   <button @click="handleClick">click meeee</button>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'Home',
 
   setup() {
-    console.log('setup')
+    console.log(this)
+
+    const para = ref(null)
+    console.log('after declare: ', para, para.value) // still has the value of null!
 
     let name = 'mario'
     let age = 30
 
     const handleClick = () => {
-      console.log('you clicked me')
+      console.log('in handleClick: ', para, para.value)
+      para.value.classList.add('test')
+      para.value.textContent = 'this now is different on click'
     }
 
     return {
       name: name,
       age: age,
       handleClick,
+      para,
     }
   },
 }
